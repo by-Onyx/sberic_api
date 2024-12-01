99e04972c33206a6ad583687f55c235daeee2e95
by_Onyx <arbit.ia@mail.ru>
2024-12-01 14:13:26 +0300

    fix set_active_location

--- app/repositories/user_location_repository.py
+++ app/repositories/user_location_repository.py
@@ -1,6 +1,8 @@
 from typing import List, Optional
 
 from sqlalchemy.orm import Session
+from sqlalchemy.testing import fails
+from websockets.version import commit
 
 from app.db.models.user_location import UserLocation
 
@@ -15,16 +17,16 @@ class UserLocationRepository:
         return db.query(UserLocation).filter_by(location_id=location_id, user_id=user_id).first() is not None
 
     def get_active_user_location(self, db: Session, user_id: int) -> Optional[int]:
-        locations = db.query(UserLocation.location_id).filter(
-            UserLocation.user_id == user_id and UserLocation.is_active == True).first()
+        locations = db.query(UserLocation.location_id).filter_by(user_id=user_id, is_active=True).first()
         return locations.location_id if locations else None
 
     def set_active_user_location(self, db: Session, user_id: int, location_id: int) -> None:
-        locations = db.query(UserLocation.location_id).filter(
+        locations = db.query(UserLocation).filter(
             UserLocation.user_id == user_id).all()
         for location in locations:
             location.is_active = location.location_id == location_id
+        db.commit()
 
     def add_user_location(self, db: Session, user_id: int, location_id: int) -> None:
-        db.add(UserLocation(user_id=user_id, location_id=location_id))
+        db.add(UserLocation(user_id=user_id, location_id=location_id, is_active=False))
         db.commit()
